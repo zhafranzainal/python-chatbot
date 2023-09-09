77232bbafad5f7d930b3f9e9995ae26d1ea6011d
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-09-10 05:42:56 +0800

    Create quote_bot.py

---
+++ Quote Chatbot/quote_bot.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+import discord
+import requests
+
+
+def get_quote():
+    response = requests.get("https://zenquotes.io/api/random")
+    json_data = json.loads(response.text)
+    quote = '"' + json_data[0]['q'] + '" - ' + json_data[0]['a']
+
+    return quote
+
+
+# get default set of intents then enable intent to receive message content
+permissions = discord.Intents.default()
+permissions.message_content = True
+
+# represent bot
+client = discord.Client(intents=permissions)
+
+
+@client.event
+async def on_ready():
+    print(f"Logged in as {client.user}")
+
+    # send message to each server it belongs to
+    for guild in client.guilds:
+        channel = guild.system_channel
+        await channel.send(f"{client.user} is online!")
+
+
+@client.event
+async def on_message(message):
+    if message.content.lower().startswith("$inspire"):
+        quote = get_quote()
+        await message.channel.send(quote)
+
+
+# Run bot using TOKEN from env
+client.run(os.environ['TOKEN'])
